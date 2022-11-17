63946566a9d45d4d0c0941bf233f932636caa9f5
Will Klein <will@willklein.co>
2022-11-16 22:31:50 -0700

    Adjust for React 18->17

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { render } from "react-dom";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
 
-root.render(
+render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  rootElement
 );
